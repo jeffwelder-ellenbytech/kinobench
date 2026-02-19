33604caf9767274854a8cdd07ab8449754cbbcb7
Jeff Welder <Jeff.Welder@ellenbytech.com>
2026-02-18 23:38:50 -0500

    feat: deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
+  // GitHub Pages project site path: https://<user>.github.io/kinobench/
+  base: '/kinobench/',
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
